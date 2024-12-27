25d0249462c1e4b07b7e32bbccd469cf035b3fd1
akkygawai <158056803+akkygawai@users.noreply.github.com>
2024-12-27 21:30:43 +0530

    Add files via upload

---
+++ mypacktest.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import mypack.mypack;
+import mypack.mypack;
+class mypacktest {
+public static void main(String args[]) {
+mypack a = new mypack();
+System.out.println("\n Result of Addition : " + a.addition (20,40));
+System.out.println("\n Result of Substraction "+ a.substraction (20,40));
+System.out.println("\n Result of Multiplication :" + a.multiplication(20,40));
+System.out.println("\n Result of Division :" + a.division(20,40)); } }
+
